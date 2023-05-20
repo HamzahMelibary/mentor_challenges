7c69ff20c07f9790b541396a333e173a57a9ceee
Hamzah Melibary <mr.hamzh@hotmail.com>
2023-05-20 20:42:17 +0300

    Complete first project web design and responsive

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+          colors: {
+      'bg-color': '#f2ebe4',
+    },
+    fontFamily: {
+      'main-font': ['Montserrat', 'sans-serif'],
+      'second-font': ['Fraunces', 'serif'],
+    },
+    },
+
+  },
+  plugins: [],
+}
